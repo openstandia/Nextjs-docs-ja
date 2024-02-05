8ffc51b94e3c2ec2e69faf43ed3f87a147297c62
NRI酒井 将大 <m2-sakai@nri.co.jp>
2024-02-05 13:23:02 +0900

    feat: install prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Redirect } from "@docusaurus/router";
-import useBaseUrl from "@docusaurus/useBaseUrl";
+import React from 'react'
+import { Redirect } from '@docusaurus/router'
+import useBaseUrl from '@docusaurus/useBaseUrl'
 
 export default function Home(): JSX.Element {
-  return <Redirect to={useBaseUrl("/docs/app-router")} />;
+  return <Redirect to={useBaseUrl('/docs/app-router')} />
 }
