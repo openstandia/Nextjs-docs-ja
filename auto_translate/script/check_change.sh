fc60d1eb8942b601fd53b6160e77c90c650cad35
kannoixia <kannoixia@gmail.com>
2024-05-28 16:44:39 +0900

    Github Actions API打鍵テスト

--- auto_translate/script/check_change.sh
+++ auto_translate/script/check_change.sh
@@ -31,9 +31,9 @@ else
 fi
 
 # 取得したshaをもとにcommit差分を取得
-response=$(curl -s -w "\n%{http_code}" --header "Authorization: token ${TOKEN}" "${DEFAULT_API_URL}/commits/${body}" | tr -d "\n")
-echo "${response}"
-body=$(echo "$response" | head -n -1 | jq ".files[].filename")
+response=$(curl -s -w "\n%{http_code}" --header "Authorization: token ${TOKEN}" "${DEFAULT_API_URL}/commits/${body}")
+body=$(echo "$response" | head -n -1 | jq -R)
+echo "${body}"
 code=$(echo "$response" | tail -n 1)
 
 if [ "$code" = "200" ]; then
