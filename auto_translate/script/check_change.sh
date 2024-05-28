d00e1434edde6ce33c61aebfd1250536d21938e0
kannoixia <kannoixia@gmail.com>
2024-05-28 17:03:42 +0900

    Github Actions API打鍵テスト

--- auto_translate/script/check_change.sh
+++ auto_translate/script/check_change.sh
@@ -19,19 +19,20 @@ TARGET_BRANCH="feature/auto-translate"
 
 # 1. github apiを使用してfeature/auto-translateブランチの変更があったファイルを取得する
 # feature/auto-translateブランチの最新commitのshaを取得する
-response=$(curl -s -w "\n%{http_code}" --header "Authorization: token ${TOKEN}" "${DEFAULT_API_URL}/branches/${TARGET_BRANCH}")
-body=$(echo "$response" | head -n -1 | jq -r ".commit.sha")
-code=$(echo "$response" | tail -n 1)
+# response=$(curl -s -w "\n%{http_code}" --header "Authorization: token ${TOKEN}" "${DEFAULT_API_URL}/branches/${TARGET_BRANCH}")
+# body=$(echo "$response" | head -n -1 | jq -r ".commit.sha")
+# code=$(echo "$response" | tail -n 1)
 
-if [ "$code" = "200" ]; then
-  echo "取得sha ${body}"
-else
-  echo "取得失敗"
-  exit 1
-fi
+# if [ "$code" = "200" ]; then
+#   echo "取得sha ${body}"
+# else
+#   echo "取得失敗"
+#   exit 1
+# fi
 
 # 取得したshaをもとにcommit差分を取得
-response=$(curl -s -w "\n%{http_code}" -H "Accept: application/vnd.github+json" -H "Authorization: token ${TOKEN}" "${DEFAULT_API_URL}/commits/${body}")
+response=$(curl -H "Accept: application/vnd.github+json" -H "Authorization: token  ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" "${DEFAULT_API_URL}/commits/${TARGET_BRANCH}")
+echo "${response}"
 body=$(echo "$response" | head -n -1 | jq ".files[].filename")
 echo "${body}"
 code=$(echo "$response" | tail -n 1)
