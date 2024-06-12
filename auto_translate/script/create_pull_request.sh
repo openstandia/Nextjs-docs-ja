5149a843613f73bba389b46c6c6fd4dd2df36b6d
kannoixia <kannoixia@gmail.com>
2024-06-12 18:12:18 +0900

    PR作成処理の追加

--- auto_translate/script/create_pull_request.sh
+++ auto_translate/script/create_pull_request.sh
@@ -10,3 +10,34 @@
 ################################################################################
 
 # 0. 環境設定
+TOKEN=$1
+DEFAULT_API_URL="https://api.github.com/repos/openstandia/Nextjs-docs-ja"
+SOURCE_BRANCH="feature/auto-translate"
+TARGET_BRANCH="feature/pr-test"
+
+
+# 1. GitHub APIを使用してPRを作成する
+# プルリクエストの内容を定義
+# GitHubリポジトリの情報を設定
+PR_TITLE="test PR title"         # プルリクエストのタイトル
+PR_BODY="test PR body"  # プルリクエストの本文
+
+
+# プルリクエストのデータをJSON形式で構築
+PR_DATA=$(jq -n \
+  --arg title "$PR_TITLE" \
+  --arg body "$PR_BODY" \
+  --arg head "$SOURCE_BRANCH" \
+  --arg base "$TARGET_BRANCH" \
+  '{title: $title, body: $body, head: $head, base: $base}')
+
+# GitHub APIを使用してプルリクエストを作成
+RESPONSE=$(curl -s -X POST -H "Authorization: token ${TOKEN}" \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -d "$PR_DATA" \
+  "${DEFAULT_API_URL}/pulls")
+
+# 作成されたプルリクエストのURLを表示
+echo "Pull Request created: $(echo "$RESPONSE" | jq -r .html_url)"
+
